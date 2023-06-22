ebe141d2b3d28f7633f9ab609b1012bd6220b3f4
howiesommerfeld <howiesommerfeld@gmail.com>
2023-06-22 21:36:13 +0200

    Use full width for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex w-full flex-col items-center justify-between p-24">
       
       <span>Welcome to Mixtape</span>
         <Image src="https://placehold.co/600x400/png" width={600} height={200} alt={''} />
